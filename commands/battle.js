602730c0fc81d2be25930695f622e412879a8d6d
Ederson Ferreira <edersondeveloper@gmail.com>
2020-10-31 23:57:34 -0400

    removing console logs and defining streamer name as profbrunolopes

--- commands/battle.js
+++ commands/battle.js
@@ -2,7 +2,7 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const fs = require("fs");
+const fs = require('fs');
 
 var canIngress = true;
 var gameStarted = false;
@@ -18,35 +18,27 @@ participants = [];
 monsterLife = 200;
 
 function battle(message, user, client, socket) {
-  const streamer = url.replace("#", "");
-  if (message == "!batalha" && user.username == "edersondeveloper") {
-    fs.writeFileSync("battle.txt", "true");
-    fs.writeFileSync("data.txt", `${new Date()}`);
-    client.action(url, "Digite !alistar para se alistar!");
+  const streamer = url.replace('#', '');
+  if (message == '!batalha' && user.username == 'profbrunolopes') {
+    fs.writeFileSync('battle.txt', 'true');
+    fs.writeFileSync('data.txt', `${new Date()}`);
+    client.action(url, 'Digite !alistar para se alistar!');
 
     gamePreStarted = true;
-    socket.emit("start", true);
+    socket.emit('start', true);
   }
-  if (message == "!alistar" && canIngress === true) {
-    if (fs.readFileSync("battle.txt", "utf8") == "true") {
+  if (message == '!alistar' && canIngress === true) {
+    if (fs.readFileSync('battle.txt', 'utf8') == 'true') {
       index = participants.indexOf(user.username);
       if (index == -1) {
         participants.push({ player: user.username, life: 100 });
       }
     }
   }
-  console.log(participants);
-  console.log(
-    message == "!atacar" &&
-      canIngress === false &&
-      gameStarted &&
-      roundPlayers &&
-      participants.findIndex((i) => i.player != -1)
-  );
 
   try {
     if (
-      message == "!atacar" &&
+      message == '!atacar' &&
       canIngress === false &&
       gameStarted &&
       roundPlayers &&
@@ -57,12 +49,12 @@ function battle(message, user, client, socket) {
       data = { player: user.username, life: monsterLife };
       client.action(
         url,
-        `${user.username} Atacou o monstro, tirando ${damage} pontos de vida!`
+        `${user.username} Atacou o monstro, tirando ${damage} pontos de vida!`,
       );
-      socket.emit("attackPlayer", JSON.stringify(data));
+      socket.emit('attackPlayer', JSON.stringify(data));
       if (monsterLife <= 0) {
         client.action(url, `O monstro morreu!`);
-        socket.emit("monsterDeath", true);
+        socket.emit('monsterDeath', true);
         gameStarted = false;
         gamePreStarted = false;
         clearInterval(gameInterval);
@@ -74,7 +66,7 @@ function battle(message, user, client, socket) {
 
   // test if the ingressTime has passed
   try {
-    let dataString = fs.readFileSync("data.txt", "utf8");
+    let dataString = fs.readFileSync('data.txt', 'utf8');
     date = new Date(dataString);
     dateNow = new Date();
     if (
@@ -84,16 +76,16 @@ function battle(message, user, client, socket) {
     ) {
       gameStarted = true;
       canIngress = false;
-      client.action(url, "Acabou o tempo. começando a batalha");
-      socket.emit("players", JSON.stringify(participants));
+      client.action(url, 'Acabou o tempo. começando a batalha');
+      socket.emit('players', JSON.stringify(participants));
     }
   } catch (err) {}
   if (gameStarted === true && intervalStarted == false) {
     intervalStarted = true;
     gameInterval = setInterval(() => {
       if (roundPlayers === false) {
-        socket.emit("round", "players");
-        client.action(url, "Round dos jogadores");
+        socket.emit('round', 'players');
+        client.action(url, 'Round dos jogadores');
 
         participants.forEach((element) => {
           client.action(url, `${element.player} Sua vez de atacar!`);
@@ -101,8 +93,8 @@ function battle(message, user, client, socket) {
 
         roundPlayers = true;
       } else {
-        socket.emit("round", "monster");
-        client.action(url, "Round do monstro");
+        socket.emit('round', 'monster');
+        client.action(url, 'Round do monstro');
 
         for (i = 0; i < participants.length; i++) {
           element = participants[i];
@@ -110,13 +102,13 @@ function battle(message, user, client, socket) {
           damage = getRndInteger(5, 10);
           client.action(
             url,
-            `O monstro tirou ${damage} pontos de vida de ${element.player}`
+            `O monstro tirou ${damage} pontos de vida de ${element.player}`,
           );
           element.life -= damage;
           if (element.life <= 0) {
             client.action(url, `${element.player} morreu!`);
 
-            socket.emit("playerDeath", element.player);
+            socket.emit('playerDeath', element.player);
             delete participants[i];
           }
         }
